f023677df78920ac7858688656acd9829261b873
Dean Chouinard <deanchouinard@gmail.com>
2016-07-21 08:55:07 -0400

    started thy_supervisor test setup

--- thy_supervisor/test/thy_supervisor_test.exs
+++ thy_supervisor/test/thy_supervisor_test.exs
@@ -1,6 +1,13 @@
+Code.load_file("thy_supervisor.exs", "../lib")
+
 defmodule ThySupervisorTest do
   use ExUnit.Case
   doctest ThySupervisor
+
+  setup do
+
+  end
+
 
   test "the truth" do
     assert 1 + 1 == 2
